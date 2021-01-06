c8124e29479cde2cb1a3e03fdf1d3819be76988a
Hunter Estrada <hestrada@ucsd.edu>
2021-01-06 00:30:01 -0800

    Added some comments to CustomFont.js

--- src/CustomFont.js
+++ src/CustomFont.js
@@ -29,7 +29,10 @@ CustomFont component accepts:
 1. font prop (string that is the FontFamily to use for the Text)
 2. styling prop (object that is the normal styles we want to apply to Text)
 
-The returned Text component will have the FontFamily attribute appended to the
-OtherStyles object, so that the FontFamily will hold precedence
+The returned Text component will have the font attribute appended to the
+styling object, so that the font will hold precedence.
+The app's global font can be changed using this component without needing to go
+thru the code base and manually change the font. Other global properties can
+also be set thru this component.
 
 */
